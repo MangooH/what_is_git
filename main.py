d329ae691e0f4dfcc9e7061172e122106fb229d5
MangooH <hmk404@yonsei.ac.kr>
2024-01-15 12:12:33 +0900

    Revert "feat: Adjust Integer Range 1, 45 to 1,50"
    This reverts commit fa5c9040aa535b2539d51cf692b18f150102fb5d.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint, choice
 
-get_randint = lambda: [randint(1, 50) for _ in range(6)]
-get_choice = lambda: [choice(range(1, 50 + 1)) for _ in range(6)]
+get_randint = lambda: [randint(1, 45) for _ in range(6)]
+get_choice = lambda: [choice(range(1, 45 + 1)) for _ in range(6)]
 
 if __name__ == "__main__":
     print(get_randint())
