fd946d345e4c1c29bd5ba1e25993582695403ea4
MangooH <hmk404@yonsei.ac.kr>
2024-01-15 12:29:24 +0900

    feat: Create foods variables

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from random import randint, choice
 get_randint = lambda: [randint(1, 45) for _ in range(6)]
 get_choice = lambda: [choice(range(1, 45 + 1)) for _ in range(6)]
 
+foods = [
+    "banana",
+    "grape",
+    "apple",
+]
+
 if __name__ == "__main__":
     print(get_randint())
     print(get_choice())
