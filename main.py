787c0425f5381e481a36d9c907a029078d8cfb99
MangooH <hmk404@yonsei.ac.kr>
2024-01-15 11:29:46 +0900

    refactor: sample > choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from random import sample
+from random import choice
 
-get_luckies = lambda: sample(range(1, 45 + 1), k=6)
+get_luckies = lambda: [choice(range(1, 45 + 1)) for _ in range(6)]
 
 if __name__ == "__main__":
     print(get_luckies())
